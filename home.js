75d905c4b1380c2614b1ccf7bd94191ccb6a1895
THXatGIT <107352477+THXatGIT@users.noreply.github.com>
2025-05-24 15:25:20 +0800

    Setup for CTF

--- home.js
+++ home.js
@@ -1,3 +1,23 @@
 function spin(){
     document.getElementById("about").getElementsByTagName("span")[0].classList.toggle("spin")
 }
+
+function swapimg1(){
+    document.getElementsByClassName("box")[1].getElementsByTagName("img")[0].src="THX2.jpeg";
+}
+function swapimg2(){
+    document.getElementsByClassName("box")[1].getElementsByTagName("img")[0].src="THX1.jpeg";
+}
+let clickcounts =0;
+function CTF(){
+    if (clickcounts<3){
+        clickcounts++
+    }
+    if (clickcounts==3){
+        document.getElementById("tbd").classList.toggle("ctf")
+        document.getElementById("c").style.color="red";document.getElementById("c").style.transform="translate(-150%)"
+        document.getElementById("t").style.color="red";document.getElementById("t").style.transform="translate(575%)"
+        document.getElementById("f").style.color="red";document.getElementById("f").style.transform="translate(200%)"
+        console.log("CTF! CTF! CTF!")
+    }
+}
